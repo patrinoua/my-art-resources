90f2b3435d1243a170e47203983b84e82adff2f9
Angela Patrinou <patrinoua@gmail.com>
2025-07-17 15:08:21 +0300

    youtube channels

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,95 @@
-import Image from "next/image";
+const videosArray = [
+  {
+    id: 1,
+    name: 'The Museum of Modern Art',
+    link: 'https://www.youtube.com/@themuseumofmodernart',
+    image: 'Moma.png',
+    dateAdded: '17/07/2025',
+  },
+  {
+    id: 2,
+    name: 'Tate',
+    link: 'https://www.youtube.com/@Tate',
+    image: 'tate.png',
+    dateAdded: '17/07/2025',
+  },
+  {
+    id: 2,
+    name: 'Luisiana Museum',
+    link: 'https://www.youtube.com/@LouisianaMuseum',
+    image: 'luisiana.png',
+    dateAdded: '17/07/2025',
+  },
+]
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div
+      className='min-h-screen flex flex-col'
+      style={{
+        background: 'linear-gradient(to bottom, #000000, #1a1a1a, #5d184cff)',
+      }}
+    >
+      <main className='flex-grow flex flex-col text-gray-200 font-sans p-8'>
+        <h1 className='text-4xl font-semibold text-center mb-10'>
+          my art resources
+        </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <nav className='flex justify-center gap-10 text-xl border-b border-gray-200 pb-4 mb-8'>
+          <span className='hover:text-white cursor-pointer'>Videos</span>
+          <span className='hover:text-white cursor-pointer'>Music</span>
+          <span className='hover:text-white cursor-pointer'>Art</span>
+        </nav>
+
+        <section className='mb-12'>
+          <h2 className='text-2xl mb-4'>Youtube channels</h2>
+          <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 '>
+            {videosArray.map((_, i) => (
+              <div key={i} className='p-4 '>
+                <a href={_.link} target='_blank' rel='noopener noreferrer'>
+                  <img
+                    src={_.image}
+                    className='h-40 mb-2 aspect-video object-contain '
+                  />
+                  <h1 className='text-xl text-gray-200 text-600 text-center'>
+                    {_.name}
+                  </h1>
+                  <p className='text-sm text-gray-200 text-center'>
+                    {_.dateAdded}
+                  </p>
+                </a>
+              </div>
+            ))}
+          </div>
+        </section>
+        {/*  
+      <section className='mb-12'>
+        <h2 className='text-2xl mb-4'>Videos</h2>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          {[...Array(3)].map((_, i) => (
+            <div key={i} className='bg-gray-800 rounded shadow-md p-4'>
+              <div className='bg-gray-600 h-40 mb-2' />
+              <p className='text-sm text-gray-400'>Name, date updated</p>
+            </div>
+          ))}
         </div>
+      </section>
+
+      <section>
+        <h2 className='text-2xl mb-4'>Instagram accounts</h2>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          {[...Array(3)].map((_, i) => (
+            <div key={i} className='bg-gray-800 rounded shadow-md p-4'>
+              <div className='bg-gray-600 h-40 mb-2' />
+              <p className='text-sm text-gray-400'>Name, date updated</p>
+            </div>
+          ))}
+        </div>
+      </section> */}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className='text-center text-sm text-gray-400 py-6 bg-orange-300'>
+        ©Angeliki Patrinou, 2025
       </footer>
     </div>
-  );
+  )
 }
